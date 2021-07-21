677f7d30f8114d348b794afb31b3203a7a7abe54
Yashiry Azize <azizeyashiry@gmail.com>
2021-07-20 21:39:08 -0400

    test

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     <nav>
-        <Link to="/travelPackages">Trek Star</Link>
+        <Link to="/travelPackages">Trek Star *</Link>
         <Link to="/travelPackages/new">Create new Package</Link>
     </nav>
 }

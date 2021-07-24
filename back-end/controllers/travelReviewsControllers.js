5c0d5e5310dc4c6a1d505ba84eafe3688609d7c7
Miguel Catala <miguelcatala27@yahoo.com>
2021-07-24 18:02:16 -0400

    travel_reviews

--- back-end/controllers/travelReviewsControllers.js
+++ back-end/controllers/travelReviewsControllers.js
@@ -2,7 +2,7 @@ const express = require('express')
 const reviews = express.Router({
     mergeParams: true
 });
-const { getAllReviews } = require('../queries/travelReviews')
+const { getAllReviews, getReview, createReview, updateReview,deleteReview } = require('../queries/travelReviews')
 
 reviews.get('/', async (req, res) => {
     const { packageId } = req.params
@@ -10,4 +10,29 @@ reviews.get('/', async (req, res) => {
     const reviews = await getAllReviews(packageId)
     res.json(reviews)
 })
+
+
+reviews.get('/:reviewId', async(req, res) =>{
+    const { reviewId } = req.params;
+    const review = await getReview(reviewId);
+    res.json(review)
+})
+
+reviews.post('/', async (req, res) => {
+    const newReview = await createReview(req.body);
+    res.json(newReview)
+})
+
+reviews.put('/:reviewId', async(req, res) => {
+    const {reviewId} = req.params;
+    const editedReview = await updateReview(reviewId);
+    res.json(editedReview);
+})
+
+reviews.delete('/:reviewId', async (req,res) =>{
+    const { reviewId} = req.params;
+    const deletedReview = await deleteReview(reviewId);
+    res.json({ success: true, payload: deletedReview });
+});
+
 module.exports = reviews
